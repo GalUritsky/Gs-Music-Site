e822621272ff10e493aacac587e3a173e9cdd667
GalUritsky <gal.uritsky@gmail.com>
2025-10-16 10:28:37 +0300

    Imported pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Home from './pages/Home'
+import About from './pages/About'
+import Credits from './pages/Credits'
 import './App.css'
 
 function App() {
