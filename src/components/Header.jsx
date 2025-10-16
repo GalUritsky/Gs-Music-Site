9255ae35290605a54151ed742fe4aa348c6020c2
GalUritsky <gal.uritsky@gmail.com>
2025-10-16 10:34:20 +0300

    Changed links to correct ones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
     <header className="bg-black text-white p-6 flex justify-between items-center fixed w-full top-0 z-50 shadow-md">
       <h1 className="text-xl font-bold">G's Music</h1>
       <nav className="space-x-6">
-        <a href="/" className="hover:text-gray-400">Home</a>
-        <a href="/releases" className="hover:text-gray-400">Releases</a>
-        <a href="/credits" className="hover:text-gray-400">Credits</a>
+        <a href="/Gs-Music-Site/" className="hover:text-gray-400">Home</a>
+        <a href="/Gs-Music-Site/#/releases" className="hover:text-gray-400">Releases</a>
+        <a href="/Gs-Music-Site/#/credits" className="hover:text-gray-400">Credits</a>
       </nav>
     </header>
   )
