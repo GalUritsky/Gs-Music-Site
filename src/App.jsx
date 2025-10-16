27dc196f5683bec9e5e4c56ee2d03b61ede069dc
GalUritsky <gal.uritsky@gmail.com>
2025-10-16 10:31:08 +0300

    Imported pages correction

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
-import About from './pages/About'
-import Credits from './pages/Credits'
+import Releases from './pages/Releases'
+import Catalouge from './pages/Catalouge'
 import './App.css'
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
       <main style={{ paddingTop: '6rem' }}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/credits" element={<Credits />} />
+          <Route path="/releases" element={<Releases />} />
+          <Route path="/catalouge" element={<Catalouge />} />
         </Routes>
       </main>
     </Router>
